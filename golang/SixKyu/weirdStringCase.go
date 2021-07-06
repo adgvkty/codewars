8d9ad6b8da03bdb333b247f183accea02c10050b
adgvkty <48160200+adgvkty@users.noreply.github.com>
2021-07-06 09:40:42 +0300

    Добавлена папка для голанг

---
+++ golang/SixKyu/weirdStringCase.go
@@ -0,0 +1,37 @@
+package kata
+
+import (
+	"strings"
+)
+
+func ToWeirdCase(str string) string {
+	var res string
+
+	// получаем []string слайс из lowercase слов
+	strSplit := strings.Split(strings.ToLower(str), " ")
+
+	// проходим в цикле по каждому слову
+	for _, v := range strSplit {
+
+		// получаем id символа в слове
+		for id := range v {
+
+			// если id символа четный
+			if id%2 == 0 {
+
+				// добавляем в строку uppercase символ
+				res += strings.ToUpper(string(v[id]))
+			} else {
+
+				// нечетный - lowercase
+				res += string(v[id])
+			}
+		}
+
+		// в конце слова добавляем пробел
+		res += " "
+	}
+
+	// убираем последний пробел
+	return res[:len(res)-1]
+}
---
+++ golang/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	six "kata/SixKyu"
+)
+
+func main() {
+	fmt.Println(six.ToWeirdCase("This is a test Looks like you passed"))
+}
