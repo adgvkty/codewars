8db5a7edd5f4c77585b28314c143d9cee039b600
adgvkty <48160200+adgvkty@users.noreply.github.com>
2021-07-06 18:34:41 +0300

    Create arithmetic.go

---
+++ golang/SevenKyu/arithmetic.go
@@ -0,0 +1,15 @@
+package seven
+
+func Arithmetic(a int, b int, operator string) int {
+	switch operator {
+	case "add":
+		return a + b
+	case "subtract":
+		return a - b
+	case "multiply":
+		return a * b
+	case "divide":
+		return a / b
+	}
+	return 0
+}
