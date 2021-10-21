d7afee589994bfbcbccd0f02c33c4337b49584ed
adgvkty <48160200+adgvkty@users.noreply.github.com>
2021-10-21 12:19:13 +0300

    sumByFactors

---
+++ golang/ForthKyu/sumByFactors.go
@@ -0,0 +1,58 @@
+package four
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+// SumOfDivided ...
+func SumOfDivided(lst []int) string {
+
+	var resultString string
+	var mapKeys []int
+	result := make(map[int]int)
+
+	for _, integer := range lst {
+		divisors := findDivisors(integer)
+		for _, divisor := range divisors {
+			result[divisor] += integer
+		}
+	}
+
+	for key := range result {
+		mapKeys = append(mapKeys, key)
+	}
+
+	sort.Ints(mapKeys)
+
+	for _, key := range mapKeys {
+		resultString += fmt.Sprintf("(%v %v)", key, result[key])
+	}
+	return resultString
+}
+
+func findDivisors(n int) []int {
+	result := []int{}
+	if n < 0 {
+		n = -n
+	}
+	for i := 2; i <= n; i++ {
+		if n%i == 0 {
+			if isPrime(i) {
+				result = append(result, i)
+			}
+
+		}
+	}
+	return result
+}
+
+func isPrime(value int) bool {
+	for i := 2; i <= int(math.Floor(float64(value)/2)); i++ {
+		if value%i == 0 {
+			return false
+		}
+	}
+	return true
+}
--- golang/main.go
+++ golang/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	six "kata/SixKyu"
+	four "kata/ForthKyu"
 )
 
 func main() {
-	fmt.Println(six.ToNato("go for it!"))
+	fmt.Println(four.ValidateSolution([][]int{
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{2, 3, 1, 5, 6, 4, 8, 9, 7},
+		{3, 1, 2, 6, 4, 5, 9, 7, 8},
+		{4, 5, 6, 7, 8, 9, 1, 2, 3},
+		{5, 6, 4, 8, 9, 7, 2, 3, 1},
+		{6, 4, 5, 9, 7, 8, 3, 1, 2},
+		{7, 8, 9, 1, 2, 3, 4, 5, 6},
+		{8, 9, 7, 2, 3, 1, 5, 6, 4},
+		{9, 7, 8, 3, 1, 2, 6, 4, 5}},
+	))
 }
